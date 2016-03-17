0ca08fa866c9abb6222764fde5788bd6e34bcd39
Kunio Sawatani <devel@fathens.org>
2016-03-17 15:30:59 +0900

    Refresher と InfiniteScroll を追加

--- app/pages/reports_list/reports_list.ts
+++ app/pages/reports_list/reports_list.ts
@@ -1,7 +1,52 @@
 import {Page} from 'ionic-angular';
+import {Observable} from 'rxjs/Rx';
 
+import {Report} from '../../model/report';
+import {Logger} from '../../providers/logging';
+
+const logger = new Logger(ReportsListPage);
 
 @Page({
-  templateUrl: 'build/pages/reports_list/reports_list.html'
+    templateUrl: 'build/pages/reports_list/reports_list.html'
 })
-export class ReportsListPage {}
+export class ReportsListPage {
+    constructor() {
+        logger.info(() => "Creating ReportsListPage");
+        this.doRefresh();
+    }
+
+    reports: Array<Report>;
+
+    doRefresh(event?) {
+        logger.info(() => `Refreshing reports list...`);
+        this.reports = [];
+        this.more().subscribe((x) => {
+            logger.debug(() => `Refreshed reports: ${x}`)
+            if (event) event.complete();
+        })
+    }
+
+    doInfinite(event) {
+        logger.debug(() => `Getting more reports: ${event}`);
+        this.more().subscribe((x) => {
+            logger.debug(() => `Generated reports: ${x}`)
+            event.complete();
+        });
+    }
+
+    private more(): Observable<any> {
+        return Observable.fromPromise(new Promise((resolve, reject) => {
+            setTimeout(() => {
+                logger.debug(() => `Generating reports`)
+                for (var i = 0; i < 10; i++) {
+                    this.reports.push(new Report(`id:${this.reports.length}`));
+                }
+                resolve(this.reports.length);
+            }, 3000);
+        }));
+    }
+
+    goReport(report: Report) {
+        logger.info(() => `Opening detail: ${report}`);
+    }
+}
