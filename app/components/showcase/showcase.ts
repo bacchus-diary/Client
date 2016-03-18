5e447a03c00f4e1a40109e824207ac515ffaeb00
Kunio Sawatani <devel@fathens.org>
2016-03-18 21:49:03 +0900

    deletePhoto のアニメーションを作成

--- app/components/showcase/showcase.ts
+++ app/components/showcase/showcase.ts
@@ -1,5 +1,6 @@
 import {IONIC_DIRECTIVES} from 'ionic-angular';
-import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {AnimationBuilder} from 'angular2/animate';
+import {Component, Input, ElementRef} from 'angular2/core';
 
 import {Leaf} from '../../model/report';
 import {PhotoShop} from '../../providers/photo_shop';
@@ -13,6 +14,8 @@ const logger = new Logger(ShowcaseComponent);
     directives: [IONIC_DIRECTIVES]
 })
 export class ShowcaseComponent {
+    constructor(private ab: AnimationBuilder, private ref: ElementRef) { }
+
     @Input() leaves: Array<Leaf>;
 
     addPhoto() {
@@ -25,7 +28,25 @@ export class ShowcaseComponent {
         });
     }
 
+    deletingIndex: number = -1;
+
     deletePhoto(index: number) {
-        logger.debug(() => `Deleting photo: ${index}`);s
+        logger.debug(() => `Deleting photo: ${index}`);
+        this.deletingIndex = index;
+
+        const target: HTMLElement = this.ref.nativeElement.querySelector(`.deletable.index-${index}`);
+        logger.debug(() => `Animate target: ${target}`);
+        if (target != null) {
+            const dur = 600;
+            const animation = this.ab.css();
+            animation.setFromStyles({ opacity: '1' });
+            animation.setToStyles({ opacity: '0', transform: 'translateY(500px)' });
+            animation.setDuration(dur);
+            animation.start(target);
+            logger.debug(() => `Animation started`);
+            setTimeout(() => {
+                this.leaves.splice(index, 1);
+            }, dur)
+        }
     }
 }
