ff66b58eb9bed0d8993fd3e9f8a6491993a52f3d
Kunio Sawatani <devel@fathens.org>
2016-03-18 16:32:15 +0900

    Showcase に写真を追加する仮実装

--- app/components/showcase/showcase.ts
+++ app/components/showcase/showcase.ts
@@ -2,6 +2,7 @@ import {IONIC_DIRECTIVES} from 'ionic-angular';
 import {Component, Input, Output, EventEmitter} from 'angular2/core';
 
 import {Leaf} from '../../model/report';
+import {PhotoShop} from '../../providers/photo_shop';
 import {Logger} from '../../providers/logging';
 
 const logger = new Logger(ShowcaseComponent);
@@ -13,4 +14,14 @@ const logger = new Logger(ShowcaseComponent);
 })
 export class ShowcaseComponent {
     @Input() leaves: Array<Leaf>;
+
+    addPhoto() {
+        PhotoShop.photo(true).subscribe((dataString) => {
+            const leaf = new Leaf();
+            const url = PhotoShop.makeUrl(PhotoShop.decodeBase64(dataString));
+            logger.debug(() => `Photo URL: ${url}`);
+            leaf.photo.reduced.mainview.url = url;
+            this.leaves.push(leaf);
+        });
+    }
 }
