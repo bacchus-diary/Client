076b531a087ef9633514de10a29c6e44d0eaf46a
Kunio Sawatani <devel@fathens.org>
2016-03-18 15:18:41 +0900

    Showcase の slides を仮実装

--- app/pages/add_report/add_report.ts
+++ app/pages/add_report/add_report.ts
@@ -2,6 +2,7 @@ import {Page} from 'ionic-angular';
 import {Observable} from 'rxjs/Rx';
 
 import {RatingComponent} from '../../components/rating/rating';
+import {ShowcaseComponent} from '../../components/showcase/showcase';
 import {Report} from '../../model/report';
 import {Logger} from '../../providers/logging';
 
@@ -9,8 +10,12 @@ const logger = new Logger(AddReportPage);
 
 @Page({
     templateUrl: 'build/pages/add_report/add_report.html',
-    directives: [RatingComponent]
+    directives: [RatingComponent, ShowcaseComponent]
 })
 export class AddReportPage {
     report = new Report("newOne");
+
+    submit() {
+        logger.debug(() => `Submitting report`);
+    }
 }
