f174dc8fbfcf26856ef35c783caa35592decff20
Kunio Sawatani <devel@fathens.org>
2016-03-23 22:15:37 +0900

    gulp-typings を導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    gulpTypings = require("gulp-typings"),
     gulpWatch = require('gulp-watch'),
     del = require('del'),
     argv = process.argv;
@@ -12,17 +13,22 @@ var buildSass = require('ionic-gulp-sass-build');
 var copyHTML = require('ionic-gulp-html-copy');
 var copyFonts = require('ionic-gulp-fonts-copy');
 
-gulp.task('watch', ['sass', 'html', 'fonts'], function(){
+gulp.task('watch', ['sass', 'html', 'fonts', 'typings'], function(){
   gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
   gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
   return buildWebpack({ watch: true });
 });
-gulp.task('build', ['sass', 'html', 'fonts'], buildWebpack);
+gulp.task('build', ['sass', 'html', 'fonts', 'typings'], buildWebpack);
 gulp.task('sass', buildSass);
 gulp.task('html', copyHTML);
 gulp.task('fonts', copyFonts);
 gulp.task('clean', function(done){
   del('www/build', done);
+});
+
+gulp.task("typings",function(){
+    gulp.src("./typings.json")
+        .pipe(gulpTypings()); //will install all typingsfiles in pipeline.
 });
 
 /**
