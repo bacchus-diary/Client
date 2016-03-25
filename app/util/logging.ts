94f7bfde250ade0b19ea3bc9d13be980e46ec3a2
Kunio Sawatani <devel@fathens.org>
2016-03-25 15:39:57 +0900

    ログにタイムスタンプを付与

--- app/util/logging.ts
+++ app/util/logging.ts
@@ -1,19 +1,48 @@
+
+function padLeft(v: string, d: number, c?: string): string {
+    if (v.length > d) return v;
+    if (!c) c = ' ';
+    return `${c.repeat(d)}${v}`.slice(-d);
+}
+
+function dateString(now?: Date): string {
+    if (!now) now = new Date();
+    const pad = (d: number) => (v: number) => padLeft(v.toString(), d, '0');
+    const date = [
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate()
+    ].map(pad(2)).join('-');
+    const time = [
+        now.getHours(),
+        now.getMinutes(),
+        now.getSeconds()
+    ].map(pad(2)).join(':');
+    return `${date} ${time}.${pad(3)(now.getMilliseconds())}`;
+}
+
+type Lebel = "DEBUG" | "INFO" | "WARN" | "FATAL";
+
 export class Logger {
-    constructor(owner: any) { }
+    constructor(private owner: any) { }
+
+    private output(lebel: Lebel, msg: string) {
+        console.log(`${dateString()}: ${padLeft(lebel, 5)}: ${msg}`);
+    }
 
     public debug(msg: () => string) {
-        console.log(msg());
+        this.output("DEBUG", msg());
     }
 
     public info(msg: () => string) {
-        console.log(msg());
+        this.output("INFO", msg());
     }
 
     public warn(msg: () => string) {
-        console.log(msg());
+        this.output("WARN", msg());
     }
 
     public fatal(msg: () => string) {
-        console.log(msg());
+        this.output("FATAL", msg());
     }
 }
