217bb068bcc0965350611e1de4f512ddcd341484
Kunio Sawatani <devel@fathens.org>
2016-04-09 14:58:06 +0900

    Suggestion に infinite-scroll を実装

--- app/components/suggestions/suggestions.ts
+++ app/components/suggestions/suggestions.ts
@@ -1,14 +1,15 @@
 import {Alert, NavController, IONIC_DIRECTIVES} from 'ionic-angular';
-import {Component, Input, Output, ElementRef, EventEmitter} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 import {Observable} from 'rxjs'
 
 import {Report} from '../../model/report';
-import {Suggestions, Product} from '../../providers/suggestions/suggestions';
+import {Suggestions, Product, PagingList} from '../../providers/suggestions/suggestions';
 import {FATHENS_PROVIDERS} from '../../providers/all';
-import {PagingList} from '../../util/pager';
 import {Logger} from '../../util/logging';
 
 const logger = new Logger(SuggestionsComponent);
+
+const scrollLastMergin = 100; // px
 
 @Component({
     selector: 'fathens-suggestions',
@@ -17,7 +18,11 @@ const logger = new Logger(SuggestionsComponent);
     providers: [FATHENS_PROVIDERS]
 })
 export class SuggestionsComponent {
-    constructor(private suggestions: Suggestions) { }
+    constructor(private suggestions: Suggestions) {    }
+
+    products: PagingList;
+
+    private _report: Report;
 
     @Input() set reloadEvent(v: Observable<void>) {
         v.subscribe(() => this.refresh());
@@ -26,31 +31,44 @@ export class SuggestionsComponent {
         this._report = v;
         this.refresh();
     }
-    private _report: Report;
 
-    products: PagingList<Product>;
+    get isLoading(): boolean {
+        return this.products.isLoading();
+    }
+
+    onScroll(event) {
+        if (!this.products.hasMore() || this.isLoading) return;
+
+        const e = event.target as HTMLDivElement;
+        const moved = e.scrollLeft;
+        const capa = e.scrollWidth - e.offsetWidth;
+        if (capa - moved > scrollLastMergin) return;
+
+        logger.debug(() => `Scrolled: ${moved}/${capa}`);
+        this.more();
+    }
 
     private async refresh() {
         if (this._report) {
             this.products = await this.suggestions.upon(this._report);
             logger.debug(() => `Setting suggestions of report`);
-            this.moreSuggestions();
+            this.more();
         } else {
             this.products = null;
         }
     }
 
-    get hasSuggestions(): boolean {
+    get isReady(): boolean {
         return this.products && this.products.list.length > 0;
     }
 
-    async moreSuggestions() {
+    async more() {
         logger.debug(() => "Start getting more suggestions...");
         await this.products.more();
         logger.debug(() => "Finish getting more suggestions...");
     }
 
-    openSuggestion(product: Product) {
+    open(product: Product) {
         this.suggestions.open(product);
     }
 }
