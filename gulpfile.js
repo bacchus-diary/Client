f371b21e7b3caecff5ab132c7ba58b09ac99beba
Kunio Sawatani <devel@fathens.org>
2016-04-11 23:20:47 +0900

    webpack 路線に変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    gulpBabel = require('gulp-babel'),
     gulpTypings = require('gulp-typings'),
     gulpWatch = require('gulp-watch'),
     del = require('del'),
@@ -29,57 +28,29 @@ gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
  * changes, but you are of course welcome (and encouraged) to customize your
  * build however you see fit.
  */
-var buildBrowserify = require('ionic-gulp-browserify-typescript');
+var webpackBuild = require('ionic-gulp-webpack');
 var buildSass = require('ionic-gulp-sass-build');
 var copyHTML = require('ionic-gulp-html-copy');
 var copyFonts = require('ionic-gulp-fonts-copy');
 var copyScripts = require('ionic-gulp-scripts-copy');
 
-var browserify = function(done, watch){
-  return function(){
-    if (watch) {
-      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
-      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-    }
-    buildBrowserify({
-        watch: watch || false,
-        outputFile: 'app.bundle.es6',
-        src: [
-            './app/app.ts',
-            './typings/main.d.ts',
-            './plugins/org.fathens.cordova.plugin.fabric.Crashlytics/www/index.d.ts'
-        ],
-        browserifyOptions: {
-            debug: false,
-            entries: [
-                './app/app.ts',
-                './typings/main.d.ts',
-                './node_modules/babel-polyfill',
-                './node_modules/reflect-metadata',
-                './plugins/org.fathens.cordova.plugin.fabric.Crashlytics/www/index.d.ts'
-            ],
-            noParse: ['./node_modules/aws-sdk', './node_modules/reflect-metadata']
-        }
-    }).on('end', () => {
-        gulp.src('www/build/js/app.bundle.es6')
-        .pipe(gulpBabel({ presets: ['es2015', 'stage-0'] }))
-        .pipe(gulp.dest('www/build/js'))
-        .on('end', done);
-    })
-  }
-}
-
 gulp.task('watch', ['clean', 'typings'], function(done){
   runSequence(
     ['sass', 'html', 'fonts', 'scripts'],
-    browserify(done, true)
+    function(){
+      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
+      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+      webpackBuild({ watch: true }).then(done);
+    }
   );
 });
 
 gulp.task('build', ['clean', 'typings'], function(done){
   runSequence(
     ['sass', 'html', 'fonts', 'scripts'],
-    browserify(done)
+    function(){
+      webpackBuild().then(done);
+    }
   );
 });
 gulp.task('sass', buildSass);
