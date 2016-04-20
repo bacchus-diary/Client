48bb2984dc7e46c8525c550a24db03b01ac8b189
Kunio Sawatani <devel@fathens.org>
2016-04-20 19:00:43 +0900

    ion-img は使わない
    リストで隠れてたところの画像はずっと表示されないなど、問題があるので今の所は使わない。

--- app/pages/reports_list/reports_list.ts
+++ app/pages/reports_list/reports_list.ts
@@ -61,11 +61,6 @@ export class ReportsListPage {
         return this.reports[index].leaves[0].photo.reduced.thumbnail.url;
     }
 
-    isLocalImage(index: number): boolean {
-        const url = this.imageUrl(index);
-        return !(_.isEmpty(url) || url.startsWith('http'));
-    }
-
     searchText: string;
     private searchTextInputing: Rx.Subscription;
 
